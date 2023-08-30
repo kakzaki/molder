bd497fa3d241fe57ffd8e267cbc25d7007d25fe5
ZAKI MUBAROK <kakzaki@gmail.com>
2023-08-30 16:53:40 +0700

    init

--- bin/molder.dart
+++ bin/molder.dart
@@ -1,6 +1,5 @@
-import 'dart:convert';
 import 'dart:io';
-import 'package:molder/molder.dart';
+import 'package:mustache_template/mustache_template.dart';
 
 void main() {
   final data = {
@@ -11,14 +10,29 @@ void main() {
     ],
   };
 
-  final generatedCode = ManualCodeGenerator.generateClass(data);
+  final templateFile = File('template/sample_template.txt');
+  final templateContent = templateFile.readAsStringSync();
+
+  final fieldsList = (data['fields'] as List<Map<String, dynamic>>)
+      .map((field) => {
+            'type': field['type'],
+            'name': field['name'],
+            'sep': (field != null && field != (data['fields'] as List).last)
+                ? ','
+                : '',
+          })
+      .toList();
+
+  final template = Template(templateContent, name: 'class_template');
+  final generatedCode = template
+      .renderString({'className': data['className'], 'fields': fieldsList});
 
   final outputDirectory = Directory('output');
   if (!outputDirectory.existsSync()) {
     outputDirectory.createSync();
   }
 
-  final outputFile = File('output/person.dart');
+  final outputFile = File('output/generated_code.dart');
   outputFile.writeAsStringSync(generatedCode);
 
   print('Code generation completed.');
--- test/molder_test.dart
+++ test/molder_test.dart
@@ -1,4 +1,3 @@
-import 'package:molder/molder.dart';
 import 'package:test/test.dart';
 
 void main() {
